f245ddda5d7b479591a84df0cdd9f545ab2232e5
puuuii <rx4_raffin@yahoo.co.jp>
2018-05-11 23:28:11 +0900

    refactor: pklからのデータロードを関数化

--- train1.py
+++ train1.py
@@ -1,13 +1,21 @@
 import pickle
+import pyworld
 
 
 def main():
+    df = load_data()
+
+
+def load_data():
+    """
+    pklからデータロード
+    :return: データ
+    """
+
     pkl_path = 'df.pkl'
     with open(pkl_path, 'rb') as pkl:
-        df = pickle.load(pkl)
-        print(df[1000:1200])
-        print(df[2000:2200])
-        print(df[30000:30200])
+        return pickle.load(pkl)
+
 
 if __name__ == '__main__':
     main()
