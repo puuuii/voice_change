eafe11277de2428cbf1ffc0ef0a7d3bd0c3c6cea
puuuii <rx4_raffin@yahoo.co.jp>
2018-05-26 17:01:12 +0900

    feat: cnnを用いた形での実装

--- makedf.py
+++ makedf.py
@@ -4,6 +4,8 @@ import pyworld as pw
 import numpy as np
 from scipy.io import wavfile as io
 from sklearn.preprocessing import StandardScaler
+from numpy import round
+from matplotlib import pylab
 
 
 FS = 16000
@@ -25,7 +27,6 @@ def main():
         try:
             sr, data_wav = io.read(path_wav)
         except FileNotFoundError:
-            print('finish')
             break
         arr_wav = pd.concat([arr_wav, pd.DataFrame(data_wav)])
 
@@ -104,36 +105,75 @@ def make_pickles(arr_all):
     wav_data = np.array(arr_all.iloc[:, 0]).astype(np.float)
     phoneme_data = arr_all.iloc[:, 1]
 
-    _f0, t = pw.dio(wav_data, FS, frame_period=3)   # 基本周波数の抽出
-    f0 = pw.stonemask(wav_data, _f0, t, FS)         # 基本周波数の修正
-    sp = pw.cheaptrick(wav_data, f0, t, FS)         # スペクトル包絡の抽出
-    ap = pw.d4c(wav_data, f0, t, FS, threshold=0.3) # 非周期性指標の抽出
+    # 各音声パラメータ取得
+    f0, sp, ap, t = get_sound_params(wav_data, 0.1, 750, 128, 0.3)
+    del wav_data
+
+    # 音素データ長を音声データ長に揃える
+    phoneme_data = list(phoneme_data)
+    phoneme_data = np.array([phoneme_data[int(np.round(time*FS))-1] for time in t])
+    del t
 
     # データの標準化
     stdsc = StandardScaler()
-    f0 = stdsc.fit_transform(f0.reshape(-1, 1)).reshape(1, -1)[0]
-    sp = stdsc.fit_transform(sp)
-    ap = stdsc.fit_transform(ap)
+    f0 = stdsc.fit_transform(f0.reshape((-1, 1))).astype(np.float32).reshape((-1))
+    sp = np.array([stdsc.fit_transform(x.reshape((-1, 1))) for x in sp], dtype=np.float32).reshape((-1, 65))
+    ap = np.array([stdsc.fit_transform(x.reshape((-1, 1))) for x in ap], dtype=np.float32).reshape((-1, 65))
 
-    # 音素データを音声データ数に合わせて抽出
-    phoneme_data = pd.concat([phoneme_data, pd.Series('sil')], ignore_index=True)
-    phoneme_data = np.array([phoneme_data.iloc[int((time * FS))] for time in t])
+    # データを800フレーム単位に分割
+    # n_split_frame = 800
+    # f0, sp, ap, phoneme_data = split_n_frame(n_split_frame, f0, sp, ap, phoneme_data)
 
-    # 基本周波数
-    with open(DIR_PICKLES + 'f0.pkl', mode='wb') as f:
-        pickle.dump(f0, f)
-
-    # スペクトル包絡
-    with open(DIR_PICKLES + 'sp.pkl', mode='wb') as f:
-        pickle.dump(sp, f, protocol=4)
-
-    # 非周期成分
-    with open(DIR_PICKLES + 'ap.pkl', mode='wb') as f:
-        pickle.dump(ap, f, protocol=4)
+    print(f0.shape, sp.shape, ap.shape, phoneme_data.shape)
 
     # 音素
     with open(DIR_PICKLES + 'phoneme.pkl', mode='wb') as f:
         pickle.dump(phoneme_data, f)
+    del phoneme_data
+
+    # 基本周波数
+    with open(DIR_PICKLES + 'f0.pkl', mode='wb') as f:
+        pickle.dump(f0, f)
+    del f0
+
+    # スペクトル包絡
+    with open(DIR_PICKLES + 'sp.pkl', mode='wb') as f:
+        pickle.dump(sp, f, protocol=4)
+    del sp
+
+    # 非周期成分
+    with open(DIR_PICKLES + 'ap.pkl', mode='wb') as f:
+        pickle.dump(ap, f, protocol=4)
+    del ap
+
+
+def get_sound_params(wav_data, frame_period, f0_floor, fft_size, threshold):
+    """
+    各音声パラメータ取得
+    """
+
+    f0, t = pw.dio(wav_data, FS, frame_period=frame_period)
+    f0 = pw.stonemask(wav_data, f0, t, FS)
+    sp = pw.cheaptrick(wav_data, f0, t, FS, f0_floor=f0_floor)
+    ap = pw.d4c(wav_data, f0, t, FS, fft_size=fft_size, threshold=threshold)
+
+    return f0, sp, ap, t
+
+
+def split_n_frame(n, f0, sp, ap, phoneme_data):
+    """
+    データをnフレーム単位に分割
+    --------------------------
+    :return:
+    """
+
+    n_max_roop = int(len(f0) / n)
+    f0 = np.array([f0[i * n:(i + 1) * n] for i in range(n_max_roop - 1)], dtype=np.float32)
+    sp = np.array([sp[i * n:(i + 1) * n] for i in range(n_max_roop - 1)], dtype=np.float32).reshape((-1, n, 65))
+    ap = np.array([ap[i * n:(i + 1) * n] for i in range(n_max_roop - 1)], dtype=np.float32).reshape((-1, n, 65))
+    phoneme_data = np.array([phoneme_data[i * n:(i + 1) * n] for i in range(n_max_roop - 1)])
+
+    return f0, sp, ap, phoneme_data
 
 
 if __name__ == '__main__':
--- train1.py
+++ train1.py
@@ -1,13 +1,10 @@
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt
+import pyworld as pw
+from scipy.io import wavfile
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.model_selection import train_test_split
-from keras.layers.core import Dense, Activation, Dropout
-from keras.layers.normalization import BatchNormalization
-from keras.optimizers import Adam
-from keras.models import Sequential
-from keras.callbacks import EarlyStopping
+from matplotlib import pyplot as plt
 
 
 DIR_PICKLES = './pickles/'
@@ -16,19 +13,20 @@ FS = 16000
 
 def main():
     # データのロード
-    f0, sp, phoneme = load_data()
+    f0, sp, ap, phoneme = load_data()
 
     # 各学習に必要な変数作成
-    x, y = make_variables(f0, sp, phoneme)
+    x, y = make_variables(f0, sp, ap, phoneme)
 
     # モデル作成
-    hidden_units = [200, 200, 200]
-    model = make_model(x, y, hidden_units)
+    hidden_units = [128, 128, 128]
+    lr = 0.01
+    model = make_model(x, y, hidden_units, lr)
 
     # 学習実行（学習済みモデルはpickle保存）
-    test_ratio = 0.3
-    n_epoch = 200
-    batch_size = 200
+    test_ratio = 0.2
+    n_epoch = 50
+    batch_size = 4096
     train_model(model, x, y, test_ratio, n_epoch, batch_size)
 
     # 予測実行
@@ -47,71 +45,77 @@ def load_data():
     with open(DIR_PICKLES + 'sp.pkl', 'rb') as f:
         sp = pickle.load(f)
 
+    # 非周期成分
+    with open(DIR_PICKLES + 'ap.pkl', 'rb') as f:
+        ap = pickle.load(f)
+
     # 音素
     with open(DIR_PICKLES + 'phoneme.pkl', 'rb') as f:
         phoneme = pickle.load(f)
+        phoneme = np.array(phoneme)
 
-    return f0, sp, phoneme
+
+    return f0, sp, ap, phoneme
 
 
-def make_variables(f0, sp, phoneme):
+def make_variables(f0, sp, ap, phoneme):
     """
     各種変数作成
     -----------------
     :param f0:      基本周波数
     :param sp:      スペクトル包絡
+    :param ap:      非周期成分
     :param phoneme: 音素
     :return:        説明変数、目的変数
     """
 
-    x = np.concatenate([np.array([f0]).T, sp], axis=1)
+    skip = 7
+    x = np.concatenate([np.array([f0]).T, sp[:, ::skip], ap[:, ::skip]], axis=1)
     y = phoneme
 
-    # onhe-hotエンコーディング実施
+    # one-hotエンコーディング実施
     le = LabelEncoder()
     y = le.fit_transform(y)
     y = np.array(y).reshape(1, -1)
     y = y.transpose()
     ohe = OneHotEncoder()
-    y = ohe.fit_transform(y).toarray()
+    y = ohe.fit_transform(y).toarray().reshape((-1, 41))
 
     return x, y
 
 
-def make_model(x, y, hidden_units):
+def make_model(x, y, hidden_units, lr):
     """
     モデル作成
     ----------
     :param x:               説明変数(f0, spの2次元)
     :param y:               目的変数(phonemeの1次元)
     :param hidden_units:    隠れ層リスト
+    :param lr:              学習率
     :return:                学習済みモデル
     """
 
+    from keras.layers.core import Activation, Dropout
+    from keras.models import Sequential
+    from keras.layers.normalization import BatchNormalization
+    from keras.layers.convolutional import Conv1D
+    from keras.layers.pooling import MaxPooling1D
 
-    # 重み初期化用関数
-    def weight_variable(shape):
-        return np.sqrt(2.0 / shape[0]) * np.random.normal(size=shape)
-
-    # モデル構築
     model = Sequential()
-    model.add(Dense(hidden_units[0], input_dim=x.shape[1], kernel_initializer=weight_variable))
+    model.add(Conv1D(128, 16, padding='same', input_shape=(1, x.shape[1])))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    for n_units in hidden_units[1:]:
-        model.add(Dense(n_units))
-        model.add(BatchNormalization())
-        model.add(Activation('relu'))
-        model.add(Dropout(0.5))
-    model.add(Dense(y.shape[1], kernel_initializer=weight_variable))
-    model.add(Activation('softmax'))
+    model.add(MaxPooling1D(2, padding='same'))
+    model.add(Conv1D(64, 8, padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling1D(2, padding='same'))
+    model.add(Conv1D(y.shape[1], 8, padding='same', activation='softmax'))
 
     model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(lr=0.01, beta_1=0.9, beta_2=0.999),
+                  optimizer='adam',
                   metrics=['accuracy'])
 
-    # モデル情報出力
     print(model.summary())
 
     return model
@@ -129,29 +133,58 @@ def train_model(model, x, y, test_ratio, n_epoch, batch_size):
     :param batch_size:  バッチサイズ
     """
 
-    # 訓練データとテストデータに分割
-    X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.1, random_state=0)
+    x = x.reshape((-1, 1, x.shape[1]))
+    y = y.reshape((-1, 1, y.shape[1]))
+    print(x.shape, y.shape)
+
+    # 訓練データと評価データに分割
+    X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=test_ratio, random_state=0)
+
 
     # 学習実行
-    hist = model.fit(X_train, Y_train, epochs=n_epoch,
-                     batch_size=batch_size,
-                     validation_split=test_ratio)
+    history = model.fit(X_train, Y_train, validation_split=0.2,
+                        batch_size=batch_size, epochs=n_epoch, verbose=1)
 
     # 予測精度出力
-    loss_and_metrics = model.evaluate(X_test, Y_test)
-    print(loss_and_metrics)
+    score = model.evaluate(X_test, Y_test)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+    # Accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 
     # モデルのpickle化
-    with open(DIR_PICKLES + 'model.pkl', mode='wb') as f:
-        pickle.dump(model, f)
+    model.save(DIR_PICKLES + 'model.h5')
 
-    # 学習の可視化
-    plt.plot(range(n_epoch), hist.history['loss'], label='loss')
-    plt.plot(range(n_epoch), hist.history['val_loss'], label='val_loss')
-    plt.xlabel('epoch')
-    plt.ylabel('loss')
-    plt.legend()
-    plt.show()
+
+def predict():
+    """
+    予測実行
+    """
+
+    path = DIR_PICKLES + 'model_481.h'
+    model = load_model(path)
+
+    fs, wav_data = wavfile.read('say.wav')
+    wav_data = wav_data.astype(np.float)
+
+    _f0, t = pw.dio(wav_data, FS)                                 # 基本周波数の抽出
+    f0 = pw.stonemask(wav_data, _f0, t, FS)                       # 基本周波数の修正
+    sp = pw.cheaptrick(wav_data, f0, t, FS, f0_floor=750)         # スペクトル包絡の抽出
 
 
 if __name__ == '__main__':
